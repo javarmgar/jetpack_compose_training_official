54627ea8deaa7cb310dde534f5b23ab958469838
Javier Armenta <j.armentagarcia@fetchrewards.com>
2023-11-15 16:49:35 -0600

    Playing a little bit with jetpack compose: Declaring a list, converting list to flow using flow{} block builder, using delay() suspend functions to simulate the creation of each item, using the cold flow to feed a hot flow also called State using State as the interface recognized by compose to read states in a lifecycle aware way to optimize resources consumtpion. Previous points were only UDF for state read, for writes we need to use events, creating event which is a lambda function, using state hoisting to move out the lambad function craetion to the host, creating the lambda function in the host, creating another state that depends on previous state, using event to update the other state, using the other state to feed another composable component

--- app/src/main/java/com/example/composetraining/MainActivity.kt
+++ app/src/main/java/com/example/composetraining/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
@@ -21,6 +20,7 @@ import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -28,9 +28,12 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.mapSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -40,6 +43,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetraining.ui.theme.ComposeTrainingTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,9 +64,20 @@ class MainActivity : ComponentActivity() {
 
 }
 
+val size = 10
+val waitingTime = 1000L
+val list: List<String> = List(size = size){ "This is the item: $it " }
+val listFlow: Flow<String> = flow<String> {
+    list.forEach {
+        delay(waitingTime)
+        emit(it)
+    }
+}
+
 @Composable
 fun App(modifier: Modifier = Modifier) {
     Column(modifier = modifier.fillMaxWidth()) {
+        ItemsScreen()
         HelloComposableStateFull()
         ArtistCardColumn(modifier = modifier)
         ArtistCardRow(modifier = modifier)
@@ -69,6 +86,67 @@ fun App(modifier: Modifier = Modifier) {
         ArtistCard2(artist = Artist())
         OffsetCard()
         WeightedCard()
+        CityScreen()
+    }
+}
+
+@Composable
+fun ItemsScreen() {
+    //1 state
+
+    val uiState by listFlow.collectAsState(initial = "Nothing collected")
+    var itemSelectedState by rememberSaveable {
+        mutableStateOf("")
+    }
+    val onTappedItem: (String) -> Unit  = { stateSelected -> itemSelectedState = stateSelected.uppercase()}
+    //2 events
+    //3 call site
+    ItemsContent(uiState, onTappedItem)
+    SelectedComponent(itemSelectedState)
+}
+
+@Composable
+fun SelectedComponent(itemSelectedState: String) {
+    Text(text = "The item selected is $itemSelectedState")
+}
+
+@Composable
+fun ItemsContent(item: String, onTappedItem: (String) -> Unit) {
+    Column {
+        Text(text = "Item: $item")
+        Button(onClick = {
+            onTappedItem(item)
+        }) {
+            Text(text = "Select item")
+        }
+
+    }
+}
+
+//@Parcelized
+//data class City(val country: String, val city: String):Parcelable
+data class City(val country: String, val city: String)
+val CitySaver = run {
+    val nameKey = "Name"
+    val countryKey = "Country"
+    mapSaver(
+        save = { mapOf(nameKey to it.city, countryKey to it.country) },
+        restore = { City(it[nameKey] as String, it[countryKey] as String) }
+    )
+}
+@Composable
+fun CityScreen() {
+    val city by rememberSaveable(stateSaver = CitySaver ) {
+        mutableStateOf(City( country = "mexico", city = "Mexico city"))
+    }
+    CityContent(city)
+}
+
+@Composable
+fun CityContent(city: City) {
+    Column {
+        Text(text = "Country: ${city.country} ")
+        Text(text = "City: ${city.city}")
     }
 }
 
@@ -211,7 +289,6 @@ fun WeightedCard() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight()
     ) {
         Icon(
             modifier = Modifier
