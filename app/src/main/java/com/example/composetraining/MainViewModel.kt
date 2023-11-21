416d45b33da80e1dddbba9b23737e9141835a5c9
Javier Armenta <j.armentagarcia@fetchrewards.com>
2023-11-20 20:59:46 -0600

    supported types of state

--- app/src/main/java/com/example/composetraining/MainViewModel.kt
+++ app/src/main/java/com/example/composetraining/MainViewModel.kt
@@ -9,16 +9,38 @@ import kotlinx.coroutines.flow.flow
 
 class MainViewModel: ViewModel() {
 
+    /*
+    Other supported types of state
+    - Compose doesn't require that you use MutableState<T> to hold state;
+      it supports other observable types.
+    - Before reading another observable type in Compose, you must convert it to a State<T>
+      so that composables can automatically recompose when the state changes
+
+     */
+
+    /*
+    Flow: collectAsStateWithLifecycle()
+    - Collects values from a Flow in a lifecycle-aware manner, allowing your app to save unneeded app resources.
+    - It represents the latest emitted value via Compose State.
+    - Use this API as the recommended way to collect flows on Android apps.
+
+    *** collectAsState()
+        -Use collectAsState for platform-agnostic code instead of collectAsStateWithLifecycle, which is Android-only.
+     */
     val size = 10
     val waitingTime = 1000L
     val list: List<String> = List(size = size){ "This is the item: $it " }
-    val listFlow: Flow<String> = flow<String> {
+    val listFlow: Flow<String> = flow {
         list.forEach {
             delay(waitingTime)
             emit(it)
         }
     }
 
+    /*
+    LiveData: observeAsState()
+    - observeAsState() starts observing this LiveData and represents its values via State.
+     */
     private val _counterLD = MutableLiveData(0)
     val counterLDState:LiveData<Int> = _counterLD
 
