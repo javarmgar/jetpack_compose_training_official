3a5dd88477a963e2f7179068b56c4528a2639669
Javier Armenta <javarmgar@gmail.com>
2023-11-22 23:24:37 -0600

    UI element State can also be hoisted to ViewModel in case it uses business logic Caveat: Don't forget coroutine Scopes in case you hoist UI element State as it is scoped to a composable

--- app/src/main/java/com/example/composetraining/StateHoistingViewModel.kt
+++ app/src/main/java/com/example/composetraining/StateHoistingViewModel.kt
@@ -20,4 +20,38 @@ class StateHoistingViewModel: ViewModel() {
         _showDetails.value = !_showDetails.value
     }
 
+
+    //UI element state
+    /*
+    You can hoist UI element state to the screen level state holder if there is business logic that needs to read or write it.
+     */
+
+    //Caveat
+    /*
+    - For some Compose UI element state,
+    - hoisting to the ViewModel might require special considerations.
+    - For example,
+    - some state holders of Compose UI elements expose methods to modify the state.
+    - Some of them might be suspend functions that trigger animations.
+    - These suspend functions can throw exceptions if you call them from a CoroutineScope that is not scoped to the Composition.
+    - viewModelScope from Compose UI causes a runtime exception of type IllegalStateException with a message reading “a MonotonicFrameClock is not available in this CoroutineContext”.
+    - To fix this, use a CoroutineScope scoped to the Composition.
+    - It provides a MonotonicFrameClock in the CoroutineContext that is necessary for the suspend functions to work.
+    - To fix this crash, switch the CoroutineContext of the coroutine in the ViewModel
+    - to one that is scoped to the Composition. It could look like this:
+    val drawerState = DrawerState(initialValue = DrawerValue.Closed)
+
+    private val _drawerContent = MutableStateFlow(DrawerContent.Empty)
+    val drawerContent: StateFlow<DrawerContent> = _drawerContent.asStateFlow()
+
+    fun closeDrawer(uiScope: CoroutineScope) {
+        viewModelScope.launch {
+            withContext(uiScope.coroutineContext) { // Use instead of the default context
+                drawerState.close()
+            }
+            // Fetch drawer content and update state
+            _drawerContent.update { content }
+        }
+    }
+     */
 }
